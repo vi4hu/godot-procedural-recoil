a9ffba1b69e9350a4f9ab902352c8b98434c3557
vi4hu <rainkun3@protonmail.com>
2024-08-18 17:34:04 +0530

    procedural recoil addon

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Change Log
+
+## 0.1.0 - 18-08-2024
+- Initial recoil code
---
+++ addons/procedural-recoil/addon.gd
@@ -0,0 +1,13 @@
+@tool
+extends EditorPlugin
+
+var icon = preload("res://addons/procedural-recoil/icon.png")
+var procedural_recoil = preload("res://addons/procedural-recoil/recoil.gd")
+
+
+func _enter_tree():
+	add_custom_type("ProceduralRecoil", "Node3D", procedural_recoil, icon)
+
+
+func _exit_tree():
+	remove_custom_type("ProceduralRecoil")
